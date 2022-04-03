e55ebf6cea4e29214cebcf3c00e1351ad8daf01a
Catalin Chitu <catalin.george.chitu@gmail.com>
2022-04-03 15:03:07 +0300

    Deleted unused files and folders, cleaned some code, almost finished

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 import { Nav } from "../../components/Nav";
-// import { HomePageProducts } from "./HomePageProducts";
 
 export function Home() {
   return (
